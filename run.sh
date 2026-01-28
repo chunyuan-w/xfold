48a11f1e47b6f0beaa5649fa41b6aac9befb9fc9
Chunyuan <chunyuan.wu@intel.com>
2026-01-29 06:10:50 +0800

    update script to collect mem usage and bandwidth

--- run.sh
+++ run.sh
@@ -23,12 +23,14 @@ JSON_PATH=/workspace/inputs
 # TODO: Jackhmmer failed
 # INPUT_NAME=titin_34350.txt
 
-# TODO: seems hang, test again
-INPUT_NAME=rydr_5038.txt
+# TODO: takes very long, test again
+# INPUT_NAME=rydr_5038.txt
 
 # INPUT_NAME=piezo2_2752.txt
 # INPUT_NAME=reelin_3469.txt
-# INPUT_NAME=lrp2_4655.txt
+INPUT_NAME=lrp2_4655.txt
+
+# TODO: failed to run since size too large
 # INPUT_NAME=lcl_6077.txt
 
 
@@ -44,13 +46,50 @@ LOG_DIR=/workspace/results/log
 
 echo "Running AlphaFold on single node for ${INPUT_NAME}, NCORES=${NCORES}, RANK=${RANK}"
 #numactl -N 0
-time numactl -N 0 python -m torch.backends.xeon.run_cpu --ninstances 1 --ncores-per-instance ${NCORES} --rank ${RANK} run_alphafold.py \
+# time numactl -N 0 python -m torch.backends.xeon.run_cpu --ninstances 1 --ncores-per-instance ${NCORES} --rank ${RANK} run_alphafold.py \
+#     --db_dir=/data \
+#     --jackhmmer_n_cpu=${NCORES} \
+#     --nhmmer_n_cpu=${NCORES} \
+#     --run_data_pipeline=False \
+#     --run_inference=True \
+#     --json_path=${JSON_PATH}/${INPUT_NAME} \
+#     --model_dir=${MODEL_DIR} \
+#     --output_dir=${OUTPUT_DIR} 2>&1 \
+#     | tee -a ${LOG_DIR}/${INPUT_NAME}.log
+
+
+
+
+
+time numactl -N 0 python -m torch.backends.xeon.run_cpu \
+    --ninstances 1 \
+    --ncores-per-instance ${NCORES} \
+    --rank ${RANK} \
+    run_alphafold.py \
     --db_dir=/data \
     --jackhmmer_n_cpu=${NCORES} \
     --nhmmer_n_cpu=${NCORES} \
-    --run_data_pipeline=True \
+    --run_data_pipeline=False \
     --run_inference=True \
     --json_path=${JSON_PATH}/${INPUT_NAME} \
     --model_dir=${MODEL_DIR} \
-    --output_dir=${OUTPUT_DIR} 2>&1 \
-    | tee -a ${LOG_DIR}/${INPUT_NAME}.log
+    --output_dir=${OUTPUT_DIR} \
+    2>&1 | tee -a ${LOG_DIR}/${INPUT_NAME}.log &
+
+PY_PID=$!
+
+# # collect RSS (memory usage)
+LOG_PREFIX=../chunyuan_profile/${INPUT_NAME}
+MEM_LOG=${LOG_PREFIX}_rss.log
+
+while true; do
+    date +%s
+    grep -E 'MemTotal|MemFree|MemAvailable|Buffers|Cached' /proc/meminfo
+    sleep 1
+done > $MEM_LOG &
+
+MEM_MONITOR_PID=$!
+echo "Memory monitor PID: $MEM_MONITOR_PID"
+
+wait $PY_PID
+kill $MEM_MONITOR_PID
